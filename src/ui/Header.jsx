c39b25bd21e44854ef964acee8e8c2a8b4b48cdc
Sa3d000n <saad_nsr@hotmail.com>
2025-07-07 02:09:32 +0300

    renaming file

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/UserName";
 
 function Header() {
   return (
-    <div className="flex justify-between bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:px-6">
+    <div className="flex justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
